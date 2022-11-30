1ee2fd1e8b3acb688a54c4afaec4fc630dc31637
Murilo Pizzamiglio <mlvieira@rpo-acad.unaerpnet.br>
2022-11-30 13:54:04 -0300

    Atualizado

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d03015ec42f73c4"; }
+/******/ 		__webpack_require__.h = function() { return "f4b01e9f2fa92e20"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
